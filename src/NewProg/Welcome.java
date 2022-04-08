778c7e48981ec2efb323d917b625464b390d862e
pravin114 <pravinsalve300@gmail.com>
2022-04-08 17:28:22 +0530

    my first edit

--- src/NewProg/Welcome.java
+++ src/NewProg/Welcome.java
@@ -5,6 +5,7 @@ public class Welcome {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("welcome to java world");
+System.out.println("i am pravin");
 	}
 
 }
